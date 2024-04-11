c33d254d03936114ab7baae08e3172f31c774fa6
Dan Koskei <qimtaid@gmail.com>
2024-04-12 01:33:16 +0300

    test with some items

--- index.js
+++ index.js
@@ -23,4 +23,13 @@ class Breakfast {
       this.dessert = dessert; // Private field for dessert
     }
   }
+
+let breakfast1 = new Breakfast("toast", "orange juice");
+let lunch1 = new Lunch("Caesar salad", "tomato soup", "iced tea");
+let dinner1 = new Dinner("green salad", "pumpkin soup", "steak", "chocolate cake");
+
+console.log(breakfast1); // Output: Breakfast { food: 'toast', drink: 'orange juice' }
+console.log(lunch1); // Output: Lunch { salad: 'Caesar salad', soup: 'tomato soup', drink: 'iced tea' }
+console.log(dinner1); // Output: Dinner { salad: 'green salad', soup: 'pumpkin soup', entree: 'steak' }
+
   
